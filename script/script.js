6438e776b6d3e37623fe4ad281c13b7c333760d2
Md. Nafiul Alam Shishir <shishirght29@gmail.com>
2026-04-02 09:06:39 +0600

    working on btn event

--- script/script.js
+++ script/script.js
@@ -1,13 +1,18 @@
 // toggling options for the job cards
 let currentTab = "all";
 
+const allContainer = document.getElementById('all-container')
+const interviewContainer = document.getElementById('interview-container')
+const rejectContainer = document.getElementById('reject-container')
+
+
+console.log(allContainer,interviewContainer,rejectContainer)
+
+// toggling tab function
 function toggleTab(tab) {
-    // const allTab = document.getElementById("all-tab");
-    // const appliedTab = document.getElementById("applied-tab");
-    // const interviewTab = document.getElementById("interview-tab");
-    // const offerTab = document.getElementById("offer-tab");
-    console.log(tab);
+    
     const tabs = ["all", "interview", "rejected"];
+    // toggle style
     for (const t of tabs) {
         const tabName = document.getElementById(`tab-${t}`);
         if (t === tab) {
@@ -18,4 +23,55 @@ function toggleTab(tab) {
             tabName.classList.add("bg-gray-300");
         }
     }
+    const pagesSection = [allContainer,interviewContainer,rejectContainer]
+    for (const pages of pagesSection) {
+        pages.classList.add('hidden')
+    }
+    // show card according to tab selected
+    if(tab ==='all')
+    {
+        allContainer.classList.remove('hidden')
+    }
+    else if(tab ==='interview')
+    {
+        interviewContainer.classList.remove('hidden')
+    }
+    else{
+        rejectContainer.classList.remove('hidden')
+    }
 }
+
+// stat update
+
+const statCount = document.getElementById('stat-total')
+const statInterview = document.getElementById('stat-interview')
+const statReject = document.getElementById('stat-reject')
+// const totalJobs = document.getElementById('total-jobs')
+
+statCount.innerText = allContainer.children.length
+// totalJobs.innerText = allContainer.children.length
+
+// call toggle function for select all tab
+
+toggleTab(currentTab)
+
+// getting btn functionatility
+
+document.getElementById('avaliable-jobs-container').addEventListener('click', function(e) {
+    const eventElemetn = e.target;
+
+    if(eventElemetn.classList.contains("interview")){
+        console.log('interview clicked')
+    }
+
+    if(eventElemetn.classList.contains("reject")){
+        console.log('reject clicked');
+    }
+
+    if(eventElemetn.classList.contains("delete")){
+        console.log('delete clicked')
+    }
+})
+
+
+
